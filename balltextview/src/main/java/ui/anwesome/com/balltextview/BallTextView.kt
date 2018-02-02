5b25dd2579b0ce095b6916b4683045255fb49059
Anwesh43 <anweshthecool0@gmail.com>
2018-02-03 03:22:42 +0530

    adding BallTextView class

--- balltextview/src/main/java/ui/anwesome/com/balltextview/BallTextView.kt
+++ balltextview/src/main/java/ui/anwesome/com/balltextview/BallTextView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.balltextview
 /**
  * Created by anweshmishra on 03/02/18.
  */
+import android.content.*
+import android.graphics.*
+import android.view.*
+class BallTextView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
