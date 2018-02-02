7dc6d94b85b692cd880254601a537700b7a213de
Anwesh43 <anweshthecool0@gmail.com>
2018-02-03 03:34:04 +0530

    adding BallText

--- balltextview/src/main/java/ui/anwesome/com/balltextview/BallTextView.kt
+++ balltextview/src/main/java/ui/anwesome/com/balltextview/BallTextView.kt
@@ -6,7 +6,7 @@ package ui.anwesome.com.balltextview
 import android.content.*
 import android.graphics.*
 import android.view.*
-class BallTextView(ctx:Context):View(ctx) {
+class BallTextView(ctx:Context,var text:String,var color:Int = Color.parseColor("#ef5350")):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
 
@@ -18,5 +18,29 @@ class BallTextView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class BallText(var size:Float,var text:String,var color:Int) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(size/2,size/2)
+            val scale = 0.1f+0.9f
+            canvas.scale(scale,scale)
+            paint.color = color
+            canvas.drawCircle(0f,0f,size/2,paint)
+            paint.textSize = size/3
+            paint.color = Color.WHITE
+            canvas.save()
+            canvas.rotate(360f)
+            canvas.scale(1f,1f)
+            canvas.drawText(text,-paint.measureText(text)/2,size/12,paint)
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
     }
 }
