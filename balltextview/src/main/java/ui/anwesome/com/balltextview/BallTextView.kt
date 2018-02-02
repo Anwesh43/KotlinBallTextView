b448193cc7ec7dca98b06625a2db5c519af27f05
Anwesh43 <anweshthecool0@gmail.com>
2018-02-03 03:48:44 +0530

    using BallTextState in BallText

--- balltextview/src/main/java/ui/anwesome/com/balltextview/BallTextView.kt
+++ balltextview/src/main/java/ui/anwesome/com/balltextview/BallTextView.kt
@@ -20,27 +20,32 @@ class BallTextView(ctx:Context,var text:String,var color:Int = Color.parseColor(
         return true
     }
     data class BallText(var size:Float,var text:String,var color:Int) {
+        val state = BallTextState()
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(size/2,size/2)
-            val scale = 0.1f+0.9f
-            canvas.scale(scale,scale)
+            state.executeForScale({
+                val scale = 0.1f+0.9f*it
+                canvas.scale(scale,scale)
+            },0)
             paint.color = color
             canvas.drawCircle(0f,0f,size/2,paint)
             paint.textSize = size/3
             paint.color = Color.WHITE
             canvas.save()
-            canvas.rotate(360f)
-            canvas.scale(1f,1f)
+            state.executeForScale({
+                canvas.rotate(360f*it)
+                canvas.scale(it,it)
+            },1)
             canvas.drawText(text,-paint.measureText(text)/2,size/9,paint)
             canvas.restore()
             canvas.restore()
         }
         fun update(stopcb:(Float)->Unit) {
-
+            state.update(stopcb)
         }
         fun startUpdating(startcb:()->Unit) {
-
+            state.startUpdating(startcb)
         }
     }
     data class BallTextState(var dir:Float = 0f,var jDir:Int = 1,var j:Int = 0,var prevScale:Float = 0f) {
