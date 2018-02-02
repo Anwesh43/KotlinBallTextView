aa27e8301570b4f87f78bde85dc0aab291883131
Anwesh43 <anweshthecool0@gmail.com>
2018-02-03 04:26:44 +0530

    creating views in activity

--- app/src/main/java/ui/anwesome/com/kotlinballtextview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinballtextview/MainActivity.kt
@@ -1,12 +1,17 @@
 package ui.anwesome.com.kotlinballtextview
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.balltextview.BallTextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallTextView.create(this,'1')
+        BallTextView.create(this,'2',Color.parseColor("#673AB7"))
+        BallTextView.create(this,'3',Color.parseColor("#00897B"))
+        BallTextView.create(this,'4',Color.parseColor("#F4511E"))
     }
 }
